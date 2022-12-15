a677abe9c7a722708b094c42dcc4e694b831f96a
Artem Subbotin <subbotin.artem@gmail.com>
2022-12-16 01:40:11 +0200

    refactor CostManager

--- contracts/CostManagerHelper.sol
+++ contracts/CostManagerHelper.sol
@@ -1,69 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./interfaces/ICostManager.sol";
-import "./interfaces/ICostManagerFactoryHelper.sol";
-import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "./CostManagerBase.sol";
 
 /**
 * used for instances that have created(cloned) by factory.
 */
-abstract contract CostManagerHelper is Initializable {
-    using AddressUpgradeable for address;
+contract CostManagerHelper is CostManagerBase {
 
-    address public costManager;
-    address public deployer;
-
-    /** 
-    * @dev sets the costmanager token
-    * @param costManager_ new address of costmanager token, or 0
-    */
-    function overrideCostManager(address costManager_) external {
-        // require factory owner or operator
-        // otherwise needed deployer(!!not contract owner) in cases if was deployed manually
-        require (
-            (deployer.isContract()) 
-                ?
-                    ICostManagerFactoryHelper(deployer).canOverrideCostManager(msg.sender, address(this))
-                :
-                    deployer == msg.sender
-            ,
-            "cannot override"
-        );
-        
-        _setCostManager(costManager_);
+    function _sender() internal override view returns(address){
+        return msg.sender;
     }
-
-    function __CostManagerHelper_init(address deployer_) internal onlyInitializing
-    {
-        deployer = deployer_;
-    }
-
-     /**
-     * @dev Private function that tells contract to account for an operation
-     * @param info uint256 The operation ID (first 8 bits). in other bits any else info
-     * @param param1 uint256 Some more information, if any
-     * @param param2 uint256 Some more information, if any
-     */
-    function _accountForOperation(uint256 info, uint256 param1, uint256 param2) internal {
-        if (costManager != address(0)) {
-            try ICostManager(costManager).accountForOperation(
-                msg.sender, info, param1, param2
-            )
-            returns (uint256 /*spent*/, uint256 /*remaining*/) {
-                // if error is not thrown, we are fine
-            } catch Error(string memory reason) {
-                // This is executed in case revert() was called with a reason
-                revert(reason);
-            } catch {
-                revert("unknown error");
-            }
-        }
-    }
-    
-    function _setCostManager(address costManager_) internal {
-        costManager = costManager_;
-    }
-    
 }
